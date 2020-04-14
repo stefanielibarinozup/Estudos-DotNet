8f3b5dd5ad8013190e237dd27424aa792477f973
stefanielibarinozup <stefanie.libarino@zup.com.br>
2020-04-13 21:32:35 -0300

    Criação das models cliente e endereço

---
+++ Desafio1-NivelBasico/Model/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio1_NivelBasico.Model
+{
+   public  class Cliente
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public int Cpf{ get; set; }
+        public int Telefone { get; set; }
+        public int Email { get; set; }
+        public Endereço Endereço{ get; set; }
+
+    }
+}
---
+++ Desafio1-NivelBasico/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desafio1_NivelBasico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
